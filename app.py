61d7093e573e6ed728a70cf644299a4e84f48318
nkpoid <nkpoidjp@gmail.com>
2022-03-01 10:35:31 +0900

    support multiple path

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import json
 import logging
 import os
 from asyncio import sleep
-from typing import Dict
+from random import choice
+from typing import Dict, List, Union
 
 import discord
 from discord import FFmpegPCMAudio, Message, VoiceClient
@@ -14,7 +15,7 @@ client = discord.Client()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("discord")
 
-SOUND_MAP: Dict
+SOUND_MAP: Dict[str, Union[str, List[str]]]
 
 
 @client.event
@@ -23,14 +24,16 @@ async def on_message(message: Message):
     if user.bot:  # type: ignore
         return
 
-    for pattern, sound_path in SOUND_MAP.items():
+    for pattern, path_candidate in SOUND_MAP.items():
         if message.content.startswith(pattern):
             if user.voice is None:
                 logger.warning("User is not on voice channel.")
                 return
 
+            src = choice(path_candidate) if isinstance(path_candidate, list) else path_candidate
+
             vc: VoiceClient = await user.voice.channel.connect()
-            vc.play(FFmpegPCMAudio(sound_path))
+            vc.play(FFmpegPCMAudio(src))
 
             while vc.is_playing():
                 await sleep(1)
