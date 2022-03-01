110a3bc3e0fe8b80dfaaa10bcdd9f6eff82235aa
nkpoid <nkpoidjp@gmail.com>
2022-03-02 03:39:06 +0900

    fix

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from random import choice
 from typing import Dict, List, Union
 
 import discord
-from discord import FFmpegPCMAudio, Message
+from discord import FFmpegPCMAudio, Member, Message
 from discord.ext import tasks
 
-client = discord.Client()
+bot = discord.Bot()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("discord")
@@ -19,21 +19,26 @@ logger = logging.getLogger("discord")
 SOUND_MAP: Dict[str, Union[str, List[str]]]
 
 
-@client.event
+@bot.event
 async def on_message(message: Message):
     if message.author.bot:
         return
 
+    if not isinstance(message.author, Member):
+        return
+
     for pattern, path_candidate in SOUND_MAP.items():
         if message.content.startswith(pattern):
-            if message.author.voice is None:  # type: ignore
+            if message.author.voice is None:
                 logger.warning("User is not on voice channel.")
                 return
 
-            src = choice(path_candidate) if isinstance(path_candidate, list) else path_candidate
+            if message.author.voice.channel is None:
+                return
 
-            vc = await message.author.voice.channel.connect()  # type:ignore
-            vc.play(FFmpegPCMAudio(src))
+            vc = await message.author.voice.channel.connect()
+            path = choice(path_candidate) if isinstance(path_candidate, list) else path_candidate
+            vc.play(FFmpegPCMAudio(path))
 
             while vc.is_playing():
                 await sleep(1)
@@ -48,4 +53,4 @@ async def config_updater():
 
 
 config_updater.start()
-client.run(os.environ.get("DISCORD_TOKEN"))
+bot.run(os.environ.get("DISCORD_TOKEN"))
